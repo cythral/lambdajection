6e682b327c4ff491738daaa8a677a4c45934f209
Talen Fisher <talen.fisher@cythral.com>
2020-08-25 09:26:30 -0500

    LAMBJ-1 Encrypted Options (#17)

---
+++ src/Generator/OptionClass.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Lambdajection.Generator
+{
+    public readonly struct OptionClass : IEquatable<OptionClass>
+    {
+        public OptionClass(string configSectionName, ClassDeclarationSyntax classDeclaration, IEnumerable<string> encryptedProperties)
+        {
+            this.ConfigSectionName = configSectionName;
+            this.ClassDeclaration = classDeclaration;
+            this.EncryptedProperties = encryptedProperties;
+        }
+
+        public string ConfigSectionName { get; }
+        public ClassDeclarationSyntax ClassDeclaration { get; }
+        public IEnumerable<string> EncryptedProperties { get; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is OptionClass result && result.ConfigSectionName.Equals(ConfigSectionName);
+        }
+
+        public bool Equals(OptionClass result)
+        {
+            return result.ConfigSectionName.Equals(ConfigSectionName);
+        }
+
+        public override int GetHashCode()
+        {
+            return ConfigSectionName.GetHashCode();
+        }
+
+        public static bool operator ==(OptionClass optionClassA, OptionClass optionClassB)
+        {
+            return optionClassA.Equals(optionClassB);
+        }
+
+        public static bool operator !=(OptionClass optionClassA, OptionClass optionClassB)
+        {
+            return !optionClassA.Equals(optionClassB);
+        }
+    }
+}
