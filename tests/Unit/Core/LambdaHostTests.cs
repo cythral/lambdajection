1eea558ed2e060c44e2730f024812b7399d7cbac
Talen Fisher <talen.fisher@cythral.com>
2020-09-21 13:12:39 -0500

    LAMBJ-43 Initialization Services (#43) [e2e]

--- tests/Unit/Core/LambdaHostTests.cs
+++ tests/Unit/Core/LambdaHostTests.cs
@@ -47,5 +47,33 @@ namespace Lambdajection.Core.Tests
             response.Should().Be(expectedResponse);
             await lambda.Received().Handle(Arg.Is(request), Arg.Is(context));
         }
+
+        [Test]
+        public async Task RunRunsInitializationServicesOnlyOnce()
+        {
+            var expectedResponse = "expectedResponse";
+            var lambda = Substitute.For<TestLambda>();
+            lambda.Handle(Arg.Any<object>(), Arg.Any<ILambdaContext>()).Returns(expectedResponse);
+
+            var initializationService = Substitute.For<ILambdaInitializationService>();
+
+            var collection = new ServiceCollection();
+            collection.AddSingleton(initializationService);
+            collection.AddSingleton(lambda);
+
+            var provider = collection.BuildServiceProvider();
+            var host = new TestLambdaHost(lambdaHost =>
+            {
+                lambdaHost.ServiceProvider = provider;
+            });
+
+            var request = new object();
+            var context = Substitute.For<ILambdaContext>();
+
+            await host.Run(request, context);
+            await host.Run(request, context);
+
+            await initializationService.Received(1).Initialize();
+        }
     }
 }
