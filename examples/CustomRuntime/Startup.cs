46c3343f6a797a1d2c2a382bb0840c3c0af1ee21
Talen Fisher <talen.fisher@cythral.com>
2020-10-14 23:34:46 -0500

    LAMBJ-88 Fix Unit Tests

--- examples/CustomRuntime/Startup.cs
+++ examples/CustomRuntime/Startup.cs
@@ -1,19 +1,11 @@
 using Lambdajection.Core;
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Lambdajection.Examples.CustomRuntime
 {
     public class Startup : ILambdaStartup
     {
-        private readonly IConfiguration configuration;
-
-        public Startup(IConfiguration configuration)
-        {
-            this.configuration = configuration;
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
             // Inject services into the Lambda's container here
