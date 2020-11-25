d1fd7cce25f7f45b68962c2bd32869851de0fcd6
Talen Fisher <talen.fisher@cythral.com>
2020-11-25 11:03:14 -0600

    LAMBJ-79 Disposable Lambda Template

--- templates/Project/Handler.cs
+++ templates/Project/Handler.cs
@@ -7,11 +7,57 @@ using Lambdajection.Attributes;
 namespace __Project__
 {
     [Lambda(typeof(Startup))]
+#if disposable
+    public partial class Handler : IAsyncDisposable, IDisposable
+#else
     public partial class Handler
+#endif
     {
+#if disposable
+        private bool disposed;
+
+#endif
         public async Task<object> Handle(object request, ILambdaContext context)
         {
             return await Task.FromResult(new { });
         }
+#if disposable
+
+        #region Disposable Methods
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsyncCore();
+            Dispose(false);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual async ValueTask DisposeAsyncCore()
+        {
+            await Task.CompletedTask;
+            // Perform asynchronous cleanup here
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+            
+            if (disposing)
+            {
+                // Perform synchronous cleanup here
+            }
+
+            disposed = true;
+        }
+        #endregion
+#endif
     }
 }
