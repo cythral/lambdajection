8a06fa2cafbc18386ad959234d54d28f8073832d
Talen Fisher <talen.fisher@cythral.com>
2021-03-02 11:23:11 -0600

    Don't Emit UpdateRequiresReplacementAttribute to Metadata

--- src/Attributes/UpdateRequiresReplacementAttribute.cs
+++ src/Attributes/UpdateRequiresReplacementAttribute.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace Lambdajection.Attributes
 {
@@ -6,6 +7,7 @@ namespace Lambdajection.Attributes
     /// Attribute used to denote properties that, when changed, cause a custom resource to be replaced.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    [Conditional("CodeGeneration")]
     public class UpdateRequiresReplacementAttribute : Attribute
     {
         /// <summary>
